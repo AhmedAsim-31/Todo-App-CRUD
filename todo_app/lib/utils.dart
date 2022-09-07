83d7917089c278cd62710604f658d78a6ff943c2
Ahmed Khan <ahmedk082@gmail.com>
2022-09-08 02:16:05 +0500

    Bug fixes

--- todo_app/lib/utils.dart
+++ todo_app/lib/utils.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 
 class Utils {
   static void showSnackBar(BuildContext context, String text) =>
-      Scaffold.of(context)
+      ScaffoldMessenger.of(context)
         ..removeCurrentSnackBar()
         ..showSnackBar(SnackBar(content: Text(text)));
 
